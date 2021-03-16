2eb9f37d70442710ea03eef5ddd153fb3bad27cd
Rusu Dinu <dinu@codingshadows.com>
2021-03-16 10:58:16 +0200

    Initial commit

---
+++ src/main/java/com/codingshadows/securemessaging/SecureMessagingApplication.java
@@ -0,0 +1,13 @@
+package com.codingshadows.securemessaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureMessagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureMessagingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingshadows/securemessaging/SecureMessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingshadows.securemessaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureMessagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
