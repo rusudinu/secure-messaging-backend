0be6381ca13f514d02b984ba7708ed5ffc9f81c5
Rusu Dinu <dinu@codingshadows.com>
2021-04-01 09:12:05 +0300

    added an api endpoint to get the number of connected users for a selected room

--- src/main/java/com/codingshadows/securemessaging/controller/MessageController.java
+++ src/main/java/com/codingshadows/securemessaging/controller/MessageController.java
@@ -1,9 +1,14 @@
 package com.codingshadows.securemessaging.controller;
 
+import com.codingshadows.securemessaging.data.ConnectionsMap;
 import com.codingshadows.securemessaging.model.Message;
+import com.codingshadows.securemessaging.model.MutableInt;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MessageController {
@@ -14,4 +19,10 @@ public class MessageController {
         return message;
     }
 
+    @GetMapping("/room/{id}/connected-count")
+    @ResponseBody
+    public MutableInt getUsersCount(@PathVariable String id) {
+        System.out.println(ConnectionsMap.connectionsCounterMap.get("/topic/messages/" + id).getValue());
+        return ConnectionsMap.connectionsCounterMap.get("/topic/messages/" + id);
+    }
 }
